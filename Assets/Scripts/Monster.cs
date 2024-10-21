389d5ca8c159c705931adcb775967e4002400c91
Helixjoe <joeljohn5112@gmail.com>
2024-10-21 17:42:08 +0530

    monster script added

---
+++ Assets/Scripts/Monster.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [HideInInspector]
+    public float speed;
+
+    private Rigidbody2D mybody;
+    void Start()
+    {
+        mybody = GetComponent<Rigidbody2D>();
+        speed=10;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        mybody.velocity = new Vector2(speed,mybody.velocity.y);
+    }
+}
