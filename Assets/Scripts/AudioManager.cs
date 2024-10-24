4a0c1dee1e53fe6d353832beaee2d19d6f9ef65f
Helixjoe <joeljohn5112@gmail.com>
2024-10-24 23:28:28 +0530

    background audio added

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField]
+    private AudioSource musicSource;
+    private AudioSource SFXSource;
+
+
+    public AudioClip Background;
+
+}
